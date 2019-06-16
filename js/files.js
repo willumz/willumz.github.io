3deda5d355256d2078cb9d5b62d6a7b5d4c0a9bb
William <15824869+Willumz@users.noreply.github.com>
2019-06-16 10:28:49 +0100

    Prevented autocorrect for mobile browsers

--- js/files.js
+++ js/files.js
@@ -4,7 +4,7 @@ files = {
     "socialmedia.txt":
         "<br>Github: <a href=\"https://github.com/willumz\">Willumz</a>",
     "ps-minifier.txt":
-        "<br>A very basic minifier for PowerShell scripts.<br><a href=\"https://github.com/willumz/ps-minifier\">Link to the Github</a>",
+        "<br>A very basic minifier for PowerShell scripts.<br><a href=\"https://github.com/willumz/ps-minifier\">Link to the Github</a> <a href=\"https://pypi.org/project/ps-minifier\">Link to the PyPi</a>",
     "keybscript.txt":
         "<br>A scripting language for keyboard input which aims to build some complexity upon the simplicity of DuckyScript.<br><a href=\"https://willumz.github.io/keybscript\">Link to the converter</a>"
 };
