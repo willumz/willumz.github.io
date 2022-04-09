e666ad4b4a69bb6b6cc4f604f58e9a38043b5b4b
Will Rycroft <willrycroft@outlook.com>
2022-04-10 00:19:35 +0100

    fix: remove animations to fix small display issues

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,65 +20,67 @@ import {
 export default class Home extends Component {
     render() {
         return (
-            <ScrollContainer>
-                <ScrollPage page={0}>
-                    <div className="absolute top-24 md:top-0 md:w-screen md:h-screen">
-                        <div className="relative md:absolute md:w-fit md:h-fit md:top-[20vh] md:left-[15%] my-7 md:my-0">
-                            <Animator animation={batch(Fade(), MoveOut(0, 300))}>
-                                <div className="md:top-[20vh] md:left-[15%] transition-transform hover:scale-110">
-                                    <SummaryCard
-                                        img={pseudoCodeLogo}
-                                        text="A simple extension for syntax highlighting of generic pseudocode."
-                                        readMoreLink="/projects"
-                                        heightClass="h-[12rem] md:h-[9rem]"
-                                        widthClass="w-[26rem] md:w-[24rem]"
-                                        disableMobileView={true}
-                                    />
-                                </div>
-                            </Animator>
-                        </div>
-                        <div className="relative md:absolute md:w-fit md:h-fit md:top-[35vh] md:right-[20%] my-7 md:my-0">
-                            <Animator animation={batch(Fade(), MoveOut(0, 200))}>
-                                <div className="transition-transform hover:scale-110">
-                                    <SummaryCard
-                                        img={cellbotLogo}
-                                        text="A command parser designed for CellBot."
-                                        readMoreLink="/projects"
-                                        heightClass="h-[12rem] md:h-[9rem]"
-                                        widthClass="w-[26rem] md:w-[22rem]"
-                                        disableMobileView={true}
-                                    />
-                                </div>
-                            </Animator>
-                        </div>
-                        <div className="relative md:absolute md:w-fit md:h-fit md:top-[55vh] md:right-[50%] my-7 md:my-0">
-                            <Animator animation={batch(Fade(), MoveOut(0, 100))}>
-                                <div className="transition-transform hover:scale-110">
-                                    <SummaryCard
-                                        img={keybscriptLogo}
-                                        text="A scripting language for keyboard input which aims to build some complexity upon the simplicity of DuckyScript."
-                                        readMoreLink="/projects"
-                                        heightClass="h-[12rem] md:h-[12rem]"
-                                        widthClass="w-[26rem] md:w-[26rem]"
-                                        disableMobileView={true}
-                                    />
-                                </div>
-                            </Animator>
-                        </div>
-                    </div>
-                </ScrollPage>
-                <ScrollPage page={1}>
-                    <div className="flex items-center justify-center h-full">
-                        <Animator animation={batch(Fade(), MoveIn(0, 700), Sticky())}>
+            // <ScrollContainer>
+            <div className="block overflow-x-hidden relative min-h-fit md:h-screen">
+                <div className="relative items-center my-24 md:my-0 md:top-0 md:w-screen md:min-h-screen min-h-fit">
+                    <div className="relative md:absolute md:w-fit h-fit md:top-[20vh] md:left-[15%] my-7 md:my-0">
+                        {/* <Animator animation={batch(Fade(), MoveOut(0, 300))}> */}
+                        <div className="transition-transform hover:scale-110">
                             <SummaryCard
-                                img={profilePic}
-                                text="About me...helloxity upon the simplicity of DuckyScript.A scripting language for keyboard input which aims to build some complexity upon the simplicity of DuckyScript."
-                                readMoreLink="/about"
+                                img={pseudoCodeLogo}
+                                text="A simple extension for syntax highlighting of generic pseudocode."
+                                readMoreLink="/projects"
+                                heightClass="h-[12rem] md:h-[9rem]"
+                                widthClass="w-[26rem] md:w-[24rem]"
+                                disableMobileView={true}
                             />
-                        </Animator>
+                        </div>
+                        {/* </Animator> */}
                     </div>
-                </ScrollPage>
-            </ScrollContainer>
+                    <div className="relative md:absolute md:w-fit h-fit md:top-[35vh] md:right-[20%] my-7 md:my-0">
+                        {/* <Animator animation={batch(Fade(), MoveOut(0, 200))}> */}
+                        <div className="transition-transform hover:scale-110">
+                            <SummaryCard
+                                img={cellbotLogo}
+                                text="A command parser designed for CellBot."
+                                readMoreLink="/projects"
+                                heightClass="h-[12rem] md:h-[9rem]"
+                                widthClass="w-[26rem] md:w-[22rem]"
+                                disableMobileView={true}
+                            />
+                        </div>
+                        {/* </Animator> */}
+                    </div>
+                    <div className="relative md:absolute md:w-fit h-fit md:top-[55vh] md:right-[50%] my-7 md:my-0">
+                        {/* <Animator animation=batch(Fade(), MoveOut(0, 100))> */}
+                        <div className="transition-transform hover:scale-110">
+                            <SummaryCard
+                                img={keybscriptLogo}
+                                text="A scripting language for keyboard input which aims to build some complexity upon the simplicity of DuckyScript."
+                                readMoreLink="/projects"
+                                heightClass="h-[12rem] md:h-[12rem]"
+                                widthClass="w-[26rem] md:w-[26rem]"
+                                disableMobileView={true}
+                            />
+                        </div>
+                        {/* </Animator> */}
+                    </div>
+                </div>
+                {/* </ScrollPage> */}
+                {/* <ScrollPage page={1}> */}
+                <div className="my-48 md:my-0 relative min-h-fit md:min-h-screen">
+                    <div className="flex items-center justify-center h-full md:min-h-screen">
+                        {/* <Animator animation={batch(Fade(), MoveIn(0, 700), Sticky())}> */}
+                        <SummaryCard
+                            img={profilePic}
+                            text="About me...helloxity upon the simplicity of DuckyScript.A scripting language for keyboard input which aims to build some complexity upon the simplicity of DuckyScript."
+                            readMoreLink="/about"
+                        />
+                        {/* </Animator> */}
+                    </div>
+                </div>
+                {/* </ScrollPage> */}
+            </div>
         );
     }
 }
